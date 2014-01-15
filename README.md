ba8b224fe2b41fa9b92926f615cedbb5748c3064
danasilver <dsilver1221@gmail.com>
2014-01-15 00:17:03 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+## Midd Events
+
+_Updated January 15, 2014_
+
+### Setup
+
+Welcome to the git repository for Midd Events.  Here are a few things to get set up if you haven't used git or Github before.
+
+ - Make sure you have git installed.  If you're not sure, run `which git` from the command line.  If you get a result (the path to the git executable), it's installed already.  For instructions on installing for your system, visit [http://git-scm.com/book/en/Getting-Started-Installing-Git](http://git-scm.com/book/en/Getting-Started-Installing-Git).  The website is also a complete resource on git.
+
+ - Get to the directory where you'll store the project, then clone (essentially copy) the repository with this command. (Don't enter the `$`.  That just means you're running this from the command line.)
+
+```sh
+$ git clone https://github.com/danasilver/midd-events.git
+```
+
+ - `cd midd-events` and get to work!
+
+### Organization
+
+Right now, the only file here is `config/db.sql`, which is the SQL used to setup our database.
---
+++ config/db.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE EventsCalendar;
+
+CREATE TABLE Users (
+     username CHAR(20) NOT NULL,
+     full_name CHAR(40) NOT NULL,
+     is_admin BOOLEAN NOT NULL DEFAULT 0,
+     joined DATETIME NOT NULL,
+     PRIMARY KEY (username)
+)
+
+CREATE TABLE Categories (
+     name CHAR(40),
+     PRIMARY KEY (name)
+)
+
+CREATE TABLE follow_cat (
+     cat CHAR(40),
+     user CHAR(20),
+     FOREIGN KEY (cat_name) REFERENCES Categories,
+     FOREIGN KEY (username) REFERENCES Users,
+     PRIMARY KEY (cat_name, username)
+)
+
+CREATE TABLE Events (
+     id INT NOT NULL AUTO_INCREMENT,
+     title CHAR(30) NOT NULL,
+     description TEXT NOT NULL,
+     is_approved BOOLEAN NOT NULL DEFAULT 0,
+     photo_url CHAR(100) NULL,
+     location CHAR(100) NOT NULL,
+     event_date DATETIME NOT NULL,
+     created_date DATETIME NOT NULL
+     host CHAR(20) NOT NULL,
+     FOREIGN KEY (host) REFERENCES Users
+     ON DELETE CASCADE,
+     cat_names CHAR(40) NOT NULL,
+     FOREIGN KEY (cat_names) REFERENCES Categories
+     ON DELETE CASCADE,
+     PRIMARY KEY (id)
+)
+
+CREATE TABLE attend (
+     user CHAR(20),
+     event INT,
+     FOREIGN KEY (user) REFERENCES Users,
+     FOREIGN KEY (event) REFERENCES Events,
+     PRIMARY KEY (user, event)
+)
+
+CREATE TABLE Organizations (
+     name CHAR(40),
+     PRIMARY KEY (name)
+)
+
+CREATE TABLE follow_org (
+     user CHAR(20),
+     org CHAR(40),
+     FOREIGN KEY (user) REFERENCES Users,
+     FOREIGN KEY (org_name) REFERENCES Organizations,
+     PRIMARY KEY (user, org_name)
+)
+
+CREATE TABLE organizer (
+     org CHAR(40),
+     event INT,
+     FOREIGN KEY (org) REFERENCES Organizations,
+     FOREIGN KEY (event) REFERENCES Events,
+     PRIMARY KEY (org, event)
+)
