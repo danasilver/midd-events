dc239b8f75fed09bfecd37ce850eb36e1fafc32a
William Moore <moorewt5@gmail.com>
2014-01-15 11:38:34 -0500

    Drill Baby Drill

---
+++ templates/event.php
@@ -0,0 +1,22 @@
+<?php
+//start PHP section
+
+//set up the connection to the database
+define('DB_SERVER', 'panther.cs.middlebury.edu')
+define('DB_USERNAME', 'dsilver')
+define('DB_PASSWORD', 'dsilver122193')
+define('DB_DATABASE', 'dsilver_EventsCalandar')
+
+$con = msql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect");
+
+<!-- Search the database -->
+<form name="search_form" method="POST" action="query.php">
+<p> Select : <select size="1" name="dropdown">
+<option value="sid">ID</option>
+<option value="sname"> name</option>
+</select> </p>
+<p> <input type="button" value="Submit" name="button_submit">
+</p>
+</form>
+
+?>
---
+++ templates/new.php
@@ -0,0 +1,27 @@
+<?php
+//start PHP section
+
+//set up the connection to the database
+define('DB_SERVER', 'panther.cs.middlebury.edu')
+define('DB_USERNAME', 'dsilver')
+define('DB_PASSWORD', 'dsilver122193')
+define('DB_DATABASE', 'dsilver_EventsCalandar')
+
+$con = msql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect");
+
+?>
+<html>
+<body>
+
+<!-- Insert into the database -->
+
+<form name="insert.php" method="post">
+ID: <input type="text" name="sid" required /> <br> <br>
+Name: <input type="text" name="sid" required /> <br> <br>
+Age: <input type="text" name="age" required /> <br> <br>
+<input type="submit" value="Insert into Database" /> <br> <br>
+
+</form>
+
+</body>
+</html>
