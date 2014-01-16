2ebfd592db3599a2c15719964374407f2e2fc5a1
Josh Pedowitz <jpedowitz@middlebury.edu>
2014-01-16 12:00:33 -0500

    debug event.php

--- templates/event.php
+++ templates/event.php
@@ -16,23 +16,23 @@ define ('DB_DATABASE', 'dsilver_EventsCalendar');
 
 $con = mysqli_connect (DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("Could not connect");
 
-$result = mysql_query("SELECT * FROM Events WHERE $_GET["Eventid"] = id");
-$event = mysql_fetch_object($result);
+$result = mysqli_query("SELECT * FROM Events WHERE $_GET["Eventid"] = id");
+$event = mysqli_fetch_array($result);
 
-Event name: echo $event -> title;
+/*Event name: echo $event -> title;
 Description: echo $event -> description;
 Where is it? echo $event -> location;
 When? echo $event -> event_date;
 Who is the host? echo $event -> host;
-$cats = mysql_query("SELECT i.category FROM categorized_in i, Events e WHERE $_GET["Eventid"] = e.id");
+$cats = mysqli_query("SELECT i.category FROM categorized_in i, Events e WHERE $_GET["Eventid"] = e.id");
 
-while ($row = mysql_fetch_field($cats))
+while ($row = mysqli_fetch_field($cats))
 {
 	echo $row['category'] . " "; 
 	echo "<br>";
 }
-
-mysql_close($con)
+*/
+mysqli_close($con);
 ?>
 
 </body>
