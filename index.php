b17d11e9de912d85493aaec81913e7d0f6a3dc0d
danasilver <dsilver1221@gmail.com>
2014-01-19 04:19:57 -0500

    Add dates to events on index and order by most recent

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ define('DB_DATABASE', 'dsilver_EventsCalendar');
 
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect.");
 
-$events_results = mysqli_query($con, "SELECT * FROM Events");
+$events_results = mysqli_query($con, "SELECT * FROM Events ORDER BY event_date DESC");
 
 $events_array = array();
 while ($row = mysqli_fetch_array($events_results, MYSQLI_ASSOC)) {
@@ -38,11 +38,20 @@ include "templates/includes/head.php"
   <p>
     <a href="new.php" class="btn btn-primary">Create an event</a>
   </p>
-  <h3>Recent Events</h3>
+  <h3>Upcoming Events</h3>
   <ul>
   <?php
   foreach ($events_array as $event) {
-    echo "<li><a href='event.php?event=" . $event['id'] . "'>"  . $event['title'] . "</a></li>";
+  ?>
+  <li>
+    <a href="event.php?event=<?php echo $event['id'] ?>">
+      <?php $phpdate = strtotime($event['event_date']) ?>
+
+      <strong><?php echo date('M j, Y', $phpdate) ?></strong>
+      &nbsp;<?php echo $event['title'] ?>
+    </a>
+  </li>
+  <?php
   }
   ?>
   </ul>
