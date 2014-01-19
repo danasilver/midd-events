357277e44159fad74e9941f4b7736c81eb320e4a
danasilver <dsilver1221@gmail.com>
2014-01-19 00:52:42 -0500

    index.php should work now, fingers crossed

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <?php
-define('DB_SERVER', 'panther.cs.middlebury.edu')
-define('DB_USERNAME', 'dsilver')
-define('DB_PASSWORD', 'dsilver122193')
-define('DB_DATABASE', 'dsilver_EventsCalendar')
+define('DB_SERVER', 'panther.cs.middlebury.edu');
+define('DB_USERNAME', 'dsilver');
+define('DB_PASSWORD', 'dsilver122193');
+define('DB_DATABASE', 'dsilver_EventsCalendar');
 
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die("Could not connect.");
 
-$sql = "SELECT * FROM Events";
+$events_query = mysqli_query($con, "SELECT * FROM Events");
+$events = mysqli_fetch_all($events_query);
 
-mysql_close($con)
+mysql_close($con);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Midd Events</title>
-<link href="static/bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<h2>Welcome to Midd Events</h2>
-<h3>Recent Events</h3>
-<ul>
-<?php
-  foreach ($sql as $value) {
-    echo "<li>".$value."</li>";
-  }
-?>
-</ul>
+<div class="container">
+  <h2>Welcome to Midd Events</h2>
+  <h3>Recent Events</h3>
+  <ul>
+  <?php
+    foreach ($events as $event) {
+      echo "<li>".$event -> title."</li>";
+    }
+  ?>
+  </ul>
+</div>
 </body>
 </html>
